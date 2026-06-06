b2703a54fce633319cff854133c583b5a8941523
xiaochaoye <2861184979@qq.com>
2026-06-06 16:08:15 +0800

    fix: 修改代码结构，修改表结构

--- src/main/java/com/shudong/ShudongApplication.java
+++ src/main/java/com/shudong/ShudongApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.shudong.mapper")
+@MapperScan({
+    "com.shudong.user.mapper",
+    "com.shudong.post.mapper",
+    "com.shudong.pick.mapper",
+    "com.shudong.message.mapper",
+    "com.shudong.admin.mapper"
+})
 public class ShudongApplication {
 
     public static void main(String[] args) {
